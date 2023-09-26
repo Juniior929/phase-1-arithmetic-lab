7b8f70837491904a0b12ad18aa0ab9315c7b42ef
+ <+>
2023-09-26 00:59:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+// 1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+// 2
+let random = Math.ceil(Math.random());
+
+// 3 
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(1, 20)
